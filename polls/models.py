7e1ef0731c50f21dd6cd9f28685362cb646dea1f
Siratee K <sirateek@gmail.com>
2021-08-30 22:44:06 +0700

    :hammer: Rename the var from 'published_date' to 'pub_date'

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    published_date = models.DateTimeField("Date Published")
+    pub_date = models.DateTimeField("Date Published")
 
     def __str__(self):
         return self.question_text
