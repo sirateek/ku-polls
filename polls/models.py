a049bee3758f2c9dac5b8470817b1e91293a04d1
Siratee K <sirateek@gmail.com>
2021-08-30 22:43:01 +0700

    Add __str__ method to 2 models to return the question, choice text

--- polls/models.py
+++ polls/models.py
@@ -5,8 +5,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     published_date = models.DateTimeField("Date Published")
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
     choice_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.choice_text
