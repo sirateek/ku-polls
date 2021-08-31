f0ba2066e5a8f448e8e7e8fdc7b83e570c3e6a0a
Siratee K <sirateek@gmail.com>
2021-09-01 00:29:48 +0700

    :bug: Remove the conflict message

--- polls/urls.py
+++ polls/urls.py
@@ -4,11 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="Index"),
     path('<int:question_id>/', views.detail, name="detail"),
-<<<<<<< HEAD
     path('<int:question_id>/vote/', views.vote, name="vote"),
     path('<int:question_id>/results/', views.result, name="results")
-=======
-    path('<int:question_id>/vote/', views.vote, name="dote"),
-    path('<int:question_id>/results/', views.result, name="desult")
->>>>>>> 7bf69614e50988aa1d8f337030bb23b0e3f4e200
 ]
