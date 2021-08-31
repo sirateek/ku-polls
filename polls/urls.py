7bf69614e50988aa1d8f337030bb23b0e3f4e200
Siratee K <sirateek@gmail.com>
2021-09-01 00:26:07 +0700

    :bug: Fix the bug that the name of view in index view template can't be found

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Index"),
-    path('<int:question_id>/', views.detail, name="Detail"),
-    path('<int:question_id>/vote/', views.vote, name="Vote"),
-    path('<int:question_id>/results/', views.result, name="Result")
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/vote/', views.vote, name="dote"),
+    path('<int:question_id>/results/', views.result, name="desult")
 ]
