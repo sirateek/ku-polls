ca39bd766ae4c8b893cb543c2119e7617ba6fb37
Siratee K <sirateek@gmail.com>
2021-08-31 14:52:19 +0700

    Add 3 more views (details, vote, results) and linked in the urlpatterns

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="Index")
+    path('', views.index, name="Index"),
+    path('<int:question_id>/', views.detail, name="Detail"),
+    path('<int:question_id>/vote/', views.vote, name="Vote"),
+    path('<int:question_id>/results/', views.result, name="Result")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,25 @@
 from django.http import HttpResponse
 
-# Create your views here.
-
 
 def index(request):
+    """Poll Index page that displays the latest few questions
+    """
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    """Question detail page that displays the question text with a form to vote
+    """
+    return HttpResponse(f"This is the question detail page for question {question_id}")
+
+
+def vote(request, question_id):
+    """Vote page that allow the user to vote on a particular question.
+    """
+    return HttpResponse(f"This is the vote page for question {question_id}")
+
+
+def result(request, question_id):
+    """Poll result page that displays the results for a particular question.
+    """
+    return HttpResponse(f"This is the result page for question {question_id}")
