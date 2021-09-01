5ba1378b4c5f11992f3e78a835433292349795b4
Siratee K <sirateek@gmail.com>
2021-09-01 23:37:59 +0700

    Implement the vote action and edit the detail page to send POST Event by using form action

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from .models import Choice, Question
+from django.urls import reverse
 
 
 def index(request):
@@ -23,7 +25,19 @@ def detail(request, question_id):
 def vote(request, question_id):
     """Vote page that allow the user to vote on a particular question.
     """
-    return HttpResponse(f"This is the vote page for question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # If the Choice doesn't not exit, Render the detail view with the `error_message`
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 def result(request, question_id):
