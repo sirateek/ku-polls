13b245c43fdd1f8c1002ca834c218a2a280ed17e
Siratee K <sirateek@gmail.com>
2021-09-04 13:02:56 +0700

    Make Question admin page be able to modify the choice related to the question.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from . import models
+from .models import Choice, Question
 
-# Register your models here.
-admin.site.register(models.Question)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ['question_text']}),
+        ('Date Information', {"fields": ['pub_date']})
+    ]
+
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
