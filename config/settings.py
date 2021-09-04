4fff0e6fa2ac7a085f94d9c9928f2904fc4fd124
Siratee K <sirateek@gmail.com>
2021-09-05 02:09:20 +0700

    Change the DEBUG setting for django to aceept the value from environment variable

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='missing-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DJANGO_DEBUG', default=True)
 
 ALLOWED_HOSTS = []
 
