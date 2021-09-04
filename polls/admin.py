a737238898b28197315cf7fd61e741781d211156
Siratee K <sirateek@gmail.com>
2021-09-04 13:08:22 +0700

    Customized the Question admin page to display the date published, published recently, and make it searchable

--- polls/admin.py
+++ polls/admin.py
@@ -12,8 +12,10 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ['question_text']}),
         ('Date Information', {"fields": ['pub_date']})
     ]
-
     inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
