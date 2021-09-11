20f71bab8f63d31b9e226419f54109b98cfdc02e
Siratee K <sirateek@gmail.com>
2021-09-11 12:01:03 +0700

    Temporary redirect the user when accessing the index page to polls app

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('polls/')),
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
 ]
