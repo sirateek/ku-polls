063a8291c6f35775e203949ae6660f768595a5ba
Siratee K <sirateek@gmail.com>
2021-09-18 12:57:54 +0700

    Update the env variable name

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='missing-secret-key')
+SECRET_KEY = env('SECRET_KEY', default='missing-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_DEBUG', default=False)
+DEBUG = env("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
