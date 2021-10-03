4e2a224530261119410f0f451c01508e055f9f3d
Siratee K <sirateek@gmail.com>
2021-10-03 22:50:24 +0700

    Add Test case for Detail view for the question that is opened and closed

--- polls/tests.py
+++ polls/tests.py
@@ -6,14 +6,15 @@ import datetime
 import json
 
 
-def create_question(question_text, days):
+def create_question(question_text, days, end_in_days=0):
     """
     Create a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
     time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+    end_date = timezone.now() + datetime.timedelta(days=end_in_days)
+    return Question.objects.create(question_text=question_text, pub_date=time, end_date=end_date)
 
 
 class QuestionIndexViewTests(TestCase):
@@ -92,13 +93,30 @@ class QuestionDetailViewTests(TestCase):
         self.assertContains(
             index_response, text=f"Question no. {future_question.id} is not accepting the vote yet.", status_code=200)
 
-    def test_past_question(self):
+    def test_past_question_not_accepting_the_vote(self):
+        """
+        Accessing detail view of a question in the pass which is stopped accepting
+        the new vote will be redirect back to the index page with the error message.
+        """
+        past_question = create_question(
+            question_text='Past Question.', days=-1, end_in_days=-1)
+        url = reverse('polls:detail', args=(past_question.id,))
+        # This response should contain the redirect code to index page.
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/polls/")
+        # The index page should contain message that question is not opened.
+        index_response = self.client.get(response.url)
+        self.assertContains(
+            index_response, text=f"Question no. {past_question.id} is not accepting the vote anymore.", status_code=200)
+
+    def test_past_question_accepting_the_vote(self):
         """
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
         past_question = create_question(
-            question_text='Past Question.', days=-5)
+            question_text='Past Question.', days=-1, end_in_days=1)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
