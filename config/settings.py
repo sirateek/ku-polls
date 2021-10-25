cb3513b2cb337be87859c263b20c78a87f96e633
Siratee K <sirateek@gmail.com>
2021-10-25 15:05:00 +0700

    Add Login redirect uri and authentication backend

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 # Read the environment vairable.
 env = environ.Env()
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,10 +108,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Authentication strategies
-AUTHENTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = [
     # username/password authentication
    'django.contrib.auth.backends.ModelBackend',  
-)
+]
+
+LOGIN_REDIRECT_URL = '/polls/' 
+
 
 
 # Internationalization
